ce4075f6a710f2a9fc5c212a3f8a074609e6b8a0
shivamanibrt <shivamanibrt03350@gmail.com>
2023-07-31 19:03:57 +1000

    initial

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 import EventEmmiter from "events";
 import express from 'express';
 import path from 'path'
+const port = 8000;
 
 const app = express();
 
 const __dirname = path.resolve();
-console.log(__dirname)
+
 app.use('/', (req, res) => {
     // res.send('<h1>Hello world</h1>')
     res.sendFile(__dirname + '/src/form.html')
 })
 
-app.listen(8000, (error) => {
+app.listen(port, (error) => {
+    console.log(`Server is running at http://localhost:${port}`)
     error && console.log(error);
-    console.log(`your server running at http://localhost:${8000}`)
-})
+});
