396b4a07d40e316c38289585bc3b8ddc09cd684e
shivamanibrt <shivamanibrt03350@gmail.com>
2023-08-01 09:53:57 +1000

    learned get and post

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-// const eventEmitter = require('events');
-import EventEmmiter from "events";
 import express from 'express';
 import path from 'path'
 const port = 8000;
 
+
 const app = express();
-
 const __dirname = path.resolve();
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.use('/', (req, res) => {
-    // res.send('<h1>Hello world</h1>')
+app.get('/registration', (req, res) => {
+    res.sendFile(__dirname + '/src/registration.html')
+})
+app.post('/registration', (req, res) => {
+    res.sendFile(__dirname + '/src/registration.html')
+})
+
+app.get('/', (req, res) => {
+    console.log(req.query, 'Post triggered')
     res.sendFile(__dirname + '/src/form.html')
 })
+app.post('/', (req, res) => {
+    console.log(req.body, 'Post triggered')
+    res.sendFile(__dirname + '/src/form.html')
+})
+
+
+
 
 app.listen(port, (error) => {
     console.log(`Server is running at http://localhost:${port}`)
