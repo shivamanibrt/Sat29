fa69c54cfb3635b636a03d214300393e04936418
shivamanibrt <shivamanibrt03350@gmail.com>
2023-08-02 09:28:27 +1000

    compared login and registration

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 import express from 'express';
-import path from 'path'
+import path from 'path';
+import fs from 'fs';
 const port = 8000;
 
 
 const app = express();
 const __dirname = path.resolve();
+
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+const fn = __dirname + '/userList.text';
 
 app.get('/registration', (req, res) => {
     res.sendFile(__dirname + '/src/registration.html')
 })
+
 app.post('/registration', (req, res) => {
-    res.sendFile(__dirname + '/src/registration.html')
+    const { email, password } = req.body;
+    const dataStr = email + "|" + password + "\n";
+
+    fs.appendFile(fn, dataStr, (error) => {
+        error && console.log(error)
+    });
+    res.sendFile(__dirname + '/src/registration.html');
 })
 
 app.get('/', (req, res) => {
@@ -20,11 +30,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/src/form.html')
 })
 app.post('/', (req, res) => {
-    console.log(req.body, 'Post triggered')
-    res.sendFile(__dirname + '/src/form.html')
+    console.log(req.body, "Clg ")
+    const { email, password } = req.body;
+    const str = email + "|" + password;
+
+    // read the file and get content as a string 
+    fs.readFile(fn, (error, data) => {
+        error && console.log(error);
+        console.log(data.toString())
+        const userLsitStr = data.toString().split("\n");
+        // console.log(userLsitStr);
+        if (userLsitStr.includes(str)) {
+            return res.send("<h1> Logged in succesfully </ h1>")
+        }
+        res.send("<h1> Invalid login </h1 > Login again")
+    })
+    // check if the incomming data exist as a string in the data
 })
-
-
 
 
 app.listen(port, (error) => {
